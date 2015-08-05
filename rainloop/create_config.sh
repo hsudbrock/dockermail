ad980c27a373c2d27dcf935d5adbcc3bebd39d12
Henning Sudbrock <henning.sudbrock@accso.de>
2015-08-05 23:23:42 +0200

    copy rainloop domain file to correct directory

--- rainloop/create_config.sh
+++ rainloop/create_config.sh
@@ -9,4 +9,6 @@ smtp_port = 587
 smtp_secure = \"TLS\"
 smtp_auth = On" > $DIR/_default_/domains/$i.ini;
  chown -R www-data:www-data $DIR/_default_/domains/$i.ini;
+ cp $DIR/_default_domains/$.ini;
 done;
+cp -r /var/www/data /var/www/html
